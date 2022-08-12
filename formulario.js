77e3e85b404c2975c0ec1de0825c883237a2d259
Nerov <75100453+Carlosjoelv@users.noreply.github.com>
2022-08-12 18:31:20 -0500

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -108,25 +108,6 @@ formulario.addEventListener('submit', (e) => {
     }
 });
 
-$('#buscar').click(function() {
-    dni = $('#documento').val();
-    $.ajax({
-        url: 'Controlador/consultaDNI.php',
-        type: 'post',
-        data: 'dni=' + dni,
-        dataType: 'json',
-        success: function(r) {
-            if (r.numeroDocumento == dni) {
-                $('#apellidoPaterno').val(r.apellidoPaterno);
-                $('#apellidoMaterno').val(r.apellidoMaterno);
-                $('#nombre').val(r.nombres);
-            } else {
-                alert(r.error);
-            }
-            console.log(r)
-        }
-    });
-});
 
 
 // ==================================== CALCULO DEL IMC  ====================================
